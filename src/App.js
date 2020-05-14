e729a618c59d9c487be7d840a34c82e211223616
Ashley Horne <ashleyhorne348@yahoo.com>
2020-05-14 09:53:58 -0400

    react-login test
    Created a login button that's toggles the user from login to logout.
    
    Has a nice background.
    
    When the user is login, the profile image will change.
    
    In the process of adding animation.
    
    will either continue to build off this or start a new mini project to improve.

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import'./styles.css';
+
+class App extends Component {
+
+
+  
+ state = {
+   isOn: false,
+   imageURL:
+    'https://www.booksie.com/files/profiles/22/mr-anonymous.png'
+  }
+
+switch = () =>{
+  this.switchImage()
+ console.log('click')
+  this.setState(state => ({
+    isOn:  !state.isOn
+  }));
+}
+
+switchImage=()=>{
+ 
+
+  if(this.state.imageURL === 'https://www.booksie.com/files/profiles/22/mr-anonymous.png')
+         this.setState({imageURL:'https://66.media.tumblr.com/2cc73ba385510f2301e5c053bdb3e6eb/tumblr_neahl0jnrF1rogx6xo1_400.png'})
+     else
+         this.setState({imageURL:'https://www.booksie.com/files/profiles/22/mr-anonymous.png'})
+  }
+
+ /*  componentDidMount =() =>{
+    setTimeout(()=>{
+      this.setState(prevState => {
+        return{
+          load: false
+        }
+      });
+    }, 5000);
+  } */
+
+render(){
+  return(
+    <div>
+      
+       <img src= {this.state.imageURL}/>
+      <h1>Your logging {this.state.isOn ? 'in':'out'}</h1>
+  <button onClick = {this.switch}>{this.state.isOn ? 'logout': 'login'}</button>
+    </div>
+  )
+}
+
+};
+export default App;
---
+++ src/loader.js
@@ -0,0 +1,12 @@
+import React, {component} from 'react';
+
+class loader extends component{
+    render =() =>{
+        return(
+            <div className ='loader'>
+                
+            
+            </div>
+        )
+    }
+}
